ea487c786434c21265067a00ab8a2005c56d6fb3
Данила Иванов <danila2003ivanov03@gmail.com>
2023-03-03 00:35:37 +0300

    test

--- AI_Lab2/AI_Lab2.cpp
+++ AI_Lab2/AI_Lab2.cpp
@@ -1,58 +1,72 @@
 ﻿#include <iostream>
-#include <string>
 #include <vector>
+#include <string>
+#include <algorithm>
 
 using namespace std;
 
-const int line = 100;
-const int column = 15;
+const int LINE = 10;
+const int COLUMN = 15;
+
+void ADD(int omega[], int main_selection[]) {
+	for (int i = 0; i < COLUMN; i++) {
+		if (main_selection[i] == 1) {
+			omega[i]++;
+		}
+	}
+	//cout << "+" << endl;
+}
+
+void SUB(int omega[], int main_selection[]) {
+	for (int i = 0; i < COLUMN; i++) {
+		if (main_selection[i] == 1) {
+			omega[i]--;
+		}
+	}
+	//cout << "-" << endl;
+}
 
 int main() {
 
 	setlocale(LC_ALL, "rus");
+	//код Иванов Данила
 
-	int teta;
-	cout << "Введите Teta: "; cin >> teta; cout << endl;
-	
-	vector<int>omega = { 1,4,2,1,6,8,3,5,1,0,11,23,6,1,6 }; //Omega
+	int y[COLUMN + 1];
+	bool flag_zero = false;
+	int temp[COLUMN] = {};
+	int teta = 0; //порог
+	int omega[COLUMN] = { -5, 3, 0, 8, 1, 2, 6, 2, -8, 4, 0, 3, 2, 1, 0 }; //омега
+	cout << "Введите Тету: "; cin >> teta; cout << endl; //ввод порога 
 
-	string path[column + 1] = {"C:\\AI_TEST\\err_lab2\\err0.txt",
-						"C:\\AI_TEST\\err_lab2\\err1.txt",
-						"C:\\AI_TEST\\err_lab2\\err2.txt",
-						"C:\\AI_TEST\\err_lab2\\err3.txt",
-						"C:\\AI_TEST\\err_lab2\\err4.txt",
-						"C:\\AI_TEST\\err_lab2\\err5.txt",
-						"C:\\AI_TEST\\err_lab2\\err6.txt",
-						"C:\\AI_TEST\\err_lab2\\err7.txt",
-						"C:\\AI_TEST\\err_lab2\\err8.txt",
-						"C:\\AI_TEST\\err_lab2\\err9.txt",
-						"C:\\AI_TEST\\err_lab2\\err10.txt",
-						"C:\\AI_TEST\\err_lab2\\err11.txt",
-						"C:\\AI_TEST\\err_lab2\\err12.txt",
-						"C:\\AI_TEST\\err_lab2\\err13.txt",
-						"C:\\AI_TEST\\err_lab2\\err14.txt",
-						"C:\\AI_TEST\\err_lab2\\err15.txt",
+	int weight = 0;
+
+	cout << "Исходная омега: | ";
+	for (int i = 0; i < COLUMN; i++) {
+		cout << omega[i] << " | ";
+	}cout << endl;
+
+	int main_selection[LINE][COLUMN] = {
+		{1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1}, //0
+		{0, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1}, //1
+		{1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1}, //2
+		{1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0}, //3
+		{1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1}, //4
+		{1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1}, //5
+		{0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1}, //6
+		{1, 1, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0}, //7
+		{1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1}, //8
+		{1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0}, //9
 	};
 
-	const int N0[column + 1][column] = {
-		{ 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1 },
-		{ 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1 }, //шаблон для числа 0
-		{ 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1 },
-		{ 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1 },
-		{ 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1 },
-		{ 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1 },
-		{ 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1 },
-		{ 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1 },
-		{ 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1 },
-		{ 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1 },
-		{ 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1 },
-		{ 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1 },
-		{ 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1 },
-		{ 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1 },
-		{ 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1 },
-		{ 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0 },
-	};
-	const int main_selection[line][column] = {}; //обучающая выборка
-
-
+	for (int iter = 0; iter < LINE; iter++) {
+		for (int point = 0; point < LINE; point++) {
+			for (int i = 0; i < COLUMN; i++) {
+				weight += omega[i] * main_selection[point][i];
+				temp[i] = main_selection[point][i];
+			}
+			if (point == 0 and weight > teta) { flag_zero = true; }
+			else if (point == 0 and weight <= teta) { ADD(omega, temp); flag_zero = false; }
+			else if (point != 0 and weight > teta) { SUB(omega, temp); flag_zero = false; }
+		}
+	}
 }
